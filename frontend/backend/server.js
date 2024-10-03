a33f50212c79e6c4263b8ffc4d835f9ccae6bd20
jessica <jessicahr1222@gmail.com>
2024-10-03 18:16:40 +1000

    vercel.json

--- frontend/backend/server.js
+++ frontend/backend/server.js
@@ -13,9 +13,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static("./public"));
+
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+
 
 app.use("/", routes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port http://localhost:${process.env.PORT}`);
