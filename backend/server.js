3407fa6ccc45064859113b5fec82c5a36746dee0
jessica <jessicahr1222@gmail.com>
2024-10-04 09:24:20 +1000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path"); // 确保 path 在任何使用之前被引入
 
 // Custom Imports
 const routes = require("./routes/routes");
@@ -14,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.use("/", routes);
 
 const ENPORT = process.env.PORT || 8080;
@@ -23,12 +23,9 @@ app.listen(ENPORT, () => {
     console.log(`Listening on port http://localhost:${process.env.PORT}`);
 });
 
-const path = require("path");
-
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 console.log(`Environment PORT: ${process.env.PORT}`);
 console.log(`Fallback ENPORT: ${ENPORT}`);
