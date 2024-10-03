b4b5f72edd1511632c20684d9a3825081b39d768
jessica <jessicahr1222@gmail.com>
2024-10-04 02:45:28 +1000

    Put the static web file "build" to /backend/public

---
+++ frontend/build/precache-manifest.81a62ae36a343e16f1b355f6a32e500f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179216e9dea930b2c9760ca4f84e68d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee666b184d3a0205ae50",
+    "url": "/static/css/main.ddff39d0.chunk.css"
+  },
+  {
+    "revision": "28c99369fd1c2a8995ef",
+    "url": "/static/js/2.7e3c2db0.chunk.js"
+  },
+  {
+    "revision": "ee666b184d3a0205ae50",
+    "url": "/static/js/main.6c960da5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
